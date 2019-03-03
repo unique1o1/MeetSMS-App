40437f3963c9f16953fe307aee9702b7fb6edb0f
Yunik Maharjan <yunik.maharjan@icloud.com>
2019-03-03 17:55:39 +0545

    optional argument added for saving cookie

--- lib/request.dart
+++ lib/request.dart
@@ -1,13 +1,8 @@
 import 'package:http/http.dart' as http;
 
-import 'package:dio/dio.dart';
 import 'dart:convert';
 
 class Session {
-  String login_url = "http://www.meet.net.np/meet/action/login";
-
-  String sms_url = "http://www.meet.net.np/meet/mod/sms/actions/send.php";
-
   Map<String, String> headers = <String, String>{};
 
   Future<Map> get(String url) async {
@@ -16,10 +11,10 @@ class Session {
     return json.decode(response.body);
   }
 
-  Future<Map> post(Map<String, String> data) async {
-    http.Response response =
-        await http.post(login_url, body: data, headers: headers);
-    updateCookie(response);
+  Future<Map> post(Map<String, String> data, String url,
+      {bool cookieBool: false}) async {
+    http.Response response = await http.post(url, body: data, headers: headers);
+    if (cookieBool) updateCookie(response);
     return json.decode(response.body);
   }
 
